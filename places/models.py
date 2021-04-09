83f70e6c472444ab78e883d72e5565fa704da075
Rahul Sharma <bbblaze27@gmail.com>
2021-04-09 19:46:08 +0530

    Create place model

--- places/models.py
+++ places/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from django.contrib.gis.db.models import PointField
+from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models here.
+
+class Place(models.Model):
+    name = models.CharField(max_length=100)
+    location = PointField(null=True)
+    description = models.TextField(max_length=3000, help_text="Enter the description of this place..")
+    address = models.CharField(max_length=500)
+    city = models.CharField(max_length=100)
+    type_of_place = models.CharField(max_length=100)
+
+    def get_absolute_url(self):
+        # pass
+        return reverse('place-detail', args=[str(self.id)])
+
+    def __str__(self):
+        return self.name
