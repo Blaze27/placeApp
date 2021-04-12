79c3d112993064bde84f516f932b8021b0db4cb4
Rahul Sharma <bbblaze27@gmail.com>
2021-04-12 14:55:29 +0530

    Update urlpatterns for matching create page and sort by cities

--- places/urls.py
+++ places/urls.py
@@ -8,3 +8,15 @@ urlpatterns = [
     path('sort_by_city', views.sortCity, name="sort_by_city"),
     path('all_places/<int:pk>', views.PlacesDetailView.as_view(), name = "place-detail"),
 ]
+
+#  Update url for adding new place
+
+urlpatterns += [
+    path('add_place', views.add_place, name="add_place"),
+]
+
+# Update url for adding distinct cities
+
+urlpatterns += [
+    path('distinct_cities/<str:cityname>', views.all_place_in_city, name="instance-city-place"),
+]
