45cb78b9d893ff73bbd38160e7fd973e22bc53c8
Rahul Sharma <bbblaze27@gmail.com>
2021-04-12 14:53:14 +0530

    Update display behavior of map in admin site

--- places/admin.py
+++ places/admin.py
@@ -3,4 +3,9 @@ from .models import Place
 from django.contrib.gis.admin import OSMGeoAdmin
 # Register your models here.
 
-admin.site.register(Place)
+# admin.site.register(Place)
+@admin.register(Place)
+class PlaceAdmin(OSMGeoAdmin):
+    default_lat = 8694755.59580
+    default_lon = 2780758.47875
+    default_zoom = 12
