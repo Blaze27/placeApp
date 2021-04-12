8b3b04f51437a8a2e8253da808b8d98a169304cc
Rahul Sharma <bbblaze27@gmail.com>
2021-04-12 14:53:49 +0530

    Create forms for using modelForm

---
+++ places/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Place
+
+class PlaceForm(forms.ModelForm):
+
+    class Meta:
+        model = Place
+        fields = (
+            'name',
+            'location', 
+            'description', 
+            'address', 
+            'city',
+            'type_of_place',
+            )
