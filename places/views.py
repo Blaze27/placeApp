07d068c87c2e93c85b672ca0b64d1efeb0616c96
Rahul Sharma <bbblaze27@gmail.com>
2021-04-12 14:56:44 +0530

    Update views for rendering sorting cities and adding new places

--- places/views.py
+++ places/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.views import generic
+from .forms import PlaceForm
+from django.contrib import messages
 # Create your views here.
 
 from places.models import Place
@@ -9,7 +11,28 @@ def index(request):
     return render(request, 'index.html')
 
 def sortCity(request):
-    return render(request, 'sortcity.html')
+    distinct_cities = Place.objects.distinct('city')
+    return render(request, 'places/sortcity.html', context={'distinct_cities' : distinct_cities})
+
+def all_place_in_city(request, cityname):
+    list_of_place = Place.objects.filter(city=cityname)
+    return render(request, 'places/sorted_by_city.html', context={'list_of_place' : list_of_place})
+
+
+
+def add_place(request):
+    if request.method == 'POST':
+        form = PlaceForm(request.POST)
+        
+        if form.is_valid():
+            x = form.save(commit=False)
+            x.save()
+            messages.success(request, 'Place added successfully.')
+            return redirect('index')
+    else:
+        form = PlaceForm()
+    
+    return render(request, 'places/add_place.html', {"form":form})
 
 class PlacesListView(generic.ListView):
     model = Place
