3bb98bf921c560fa4b43ecf00c6a2c83be2212dc
Rahul Sharma <bbblaze27@gmail.com>
2021-04-12 15:53:34 +0530

    Update project settings for heroku deployment

--- placeApp/settings.py
+++ placeApp/settings.py
@@ -17,17 +17,15 @@ from django.contrib.messages import constants as messages
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
+
 # SECRET_KEY = 'jv_5(mlt+8l8*irl(o_ywr+#3q(pei@^s)95o%h_g#1d-8ir_i'
-
-# # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG')
+
 
 ALLOWED_HOSTS = ['tp-placeapp.herokuapp.com', '127.0.0.1']
 
@@ -81,6 +79,17 @@ WSGI_APPLICATION = 'placeApp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'my_db',
+#         'USER' : 'rahul',
+#         'PASSWORD' : 'admin',
+#         'HOST' : '127.0.0.1',
+#         'PORT' : '5432', 
+#     }
+# }
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.contrib.gis.db.backends.postgis',
@@ -130,3 +139,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
