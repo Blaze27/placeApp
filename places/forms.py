10f4775300eb3146770b2f2adbb6849832ec7130
Rahul Sharma <bbblaze27@gmail.com>
2021-04-20 12:33:02 +0530

    Update forms for selecting location accurately

--- places/forms.py
+++ places/forms.py
@@ -1,16 +1,18 @@
 from django.contrib.gis import forms
 from .models import Place
-
-
+from leaflet.forms.widgets import LeafletWidget
+from django import forms
 
 class PlaceForm(forms.ModelForm):
     class Meta:
         model = Place
         fields = (
             'name', 
+            'location',
             'description', 
             'address', 
             'city',
             'type_of_place',
-            'location',
+            
             )
+        widgets = {'location' : LeafletWidget()}
